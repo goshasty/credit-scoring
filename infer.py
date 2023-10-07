a1d9c79d41f496bbf75e4d27c3736618b2b03858
georgy.demin <georgy.demin@lamoda.ru>
2023-10-07 11:54:28 +0300

    f

--- infer.py
+++ infer.py
@@ -1,15 +1,16 @@
 import pickle
 
+import pandas as pd
 from sklearn import datasets
-from sklearn.linear_model import LogisticRegression
 
 if __name__ == "__main__":
     iris = datasets.load_iris()
-    X = iris.data[:, :2]
-    Y = iris.target
-    logreg = LogisticRegression(C=1e5)
-    logreg.fit(X, Y)
-    pickle.dump(logreg, open("model.sav", "wb"))
+    X = iris.data[1::2, :2]
+    Y = iris.target[1::2]
     logreg = pickle.load(open("model.sav", "rb"))
+    res = logreg.predict(X)
+
+    res = pd.DataFrame(columns=["preds"], data=res)
+    res.to_csv("answers.csv")
 
     print("Score {0:.2f}".format(logreg.score(X, Y)))

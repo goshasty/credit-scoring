3bfa1f107e2096a2ff4608d846724a6d59940b7f
georgy.demin <georgy.demin@lamoda.ru>
2023-11-28 19:53:29 +0530

    lgb does not work on mac

--- commands.py
+++ commands.py
@@ -1,2 +1,5 @@
+from credit_scoring.train import hello
+
 if __name__ == "__main__":
     print("Hello world!")
+    hello()
---
+++ credit_scoring/train.py
@@ -0,0 +1,5 @@
+import lightgbm as lgb
+
+
+def hello():
+    print(lgb.__version__)

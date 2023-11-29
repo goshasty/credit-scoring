223d76b41c42130ab0787dc88574c3ad01ca050d
goshasty <goshasty@yandex.ru>
2023-11-29 07:25:29 +0000

    hw_1

--- commands.py
+++ commands.py
@@ -1,5 +1,46 @@
-from credit_scoring.train import hello
+import logging
+
+import fire
+
+from credit_scoring.infer import LGBMInfer
+from credit_scoring.train import LGBMFit
+from credit_scoring.utils import load_data, logging_setup, save_predicts
+
+
+@logging_setup
+def train(
+    path_train_data: str = "data/train.csv",
+    path_fitted_model: str = "models/v1.pickle",
+):
+    logging.info(path_train_data)
+    logging.info(path_fitted_model)
+
+    train_data = load_data(path_train_data)
+    fitter = LGBMFit(path_fitted_model)
+
+    logging.info("Start fitting")
+    lgb_classifier = fitter.fit_boosting(train_data)
+    fitter.save_model(lgb_classifier)
+
+    logging.info("Model was saved")
+
+
+@logging_setup
+def infer(
+    path_infer_data: str = "data/X_test.csv",
+    path_save_predicts: str = "data/test_probas.csv",
+    path_fitted_model: str = "models/v1.pickle",
+):
+    logging.info(path_fitted_model)
+
+    infer_data = load_data(path_infer_data)
+    infer = LGBMInfer(path_fitted_model)
+
+    logging.info("Start predicting")
+    predicts = infer.predict_proba(infer_data, 1)
+
+    save_predicts(predicts, path_save_predicts)
+
 
 if __name__ == "__main__":
-    print("Hello world!")
-    hello()
+    fire.Fire()
--- credit_scoring/train.py
+++ credit_scoring/train.py
@@ -1,5 +1,46 @@
-import lightgbm as lgb
+from lightgbm import LGBMClassifier
+
+from credit_scoring.preprocessing import GetX
+
+from .utils import save_model
 
 
-def hello():
-    print(lgb.__version__)
+class LGBMFit:
+    def __init__(self, path_fitted_model):
+        self.params = {
+            "objective": "binary",
+            "metric": "binary_logloss",
+            "boosting_type": "gbdt",
+            "num_leaves": 31,
+            "learning_rate": 0.05,
+            "feature_fraction": 0.9,
+            "n_estimators": 5,  # 100
+        }
+
+        self.y_col = "target"
+        self.ts_col = "ts"
+        self.tech_cols = [self.y_col, self.ts_col]
+
+        self.path_fitted_model = path_fitted_model
+
+    def __fit_boosting(self, X_train, y_train, params):
+        lgb_classifier = LGBMClassifier(**self.params)
+        lgb_classifier.fit(
+            X_train,
+            y_train
+            # eval_set=[(X_test_pca, y_test)],
+            # callbacks=[early_stopping(10)],
+        )
+
+        # Making predictions
+        return lgb_classifier
+
+    def fit_boosting(self, train_data, params=None):
+        params = params if params is not None else self.params
+        X_train = GetX(self.tech_cols).fit_transform(train_data)
+        y_train = train_data[self.y_col]
+
+        return self.__fit_boosting(X_train, y_train, params)
+
+    def save_model(self, lgb_classifier):
+        save_model(lgb_classifier, self.path_fitted_model)

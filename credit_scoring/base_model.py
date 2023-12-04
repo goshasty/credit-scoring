d578fde49f86e2de8bf3f848e1cd2988c98652a1
gdemin <goshasty@yandex.ru>
2023-12-04 16:31:31 +0300

    infer to parent class

--- credit_scoring/base_model.py
+++ credit_scoring/base_model.py
@@ -1,6 +1,11 @@
+import logging
 from typing import List
 
+import numpy as np
+import pandas as pd
 from omegaconf import OmegaConf
+
+from credit_scoring.preprocessing import GetX
 
 
 class LGBM:
@@ -8,3 +13,22 @@ class LGBM:
         self.params = lgbt_params
         self.cols = cols
         self.tech_cols = [col for _, col in self.cols.items()]
+
+    def predict_proba(
+        self, X_test: pd.DataFrame, target_class=None
+    ) -> np.ndarray:
+        X_test = GetX(self.tech_cols).fit_transform(X_test)
+        propability = self.model.predict_proba(X_test)
+
+        logging.info(f"Len of data is {len(propability)}")
+
+        if target_class is None:
+            return propability
+        elif target_class == 0:
+            return propability[:, 0]
+        elif target_class == 1:
+            return propability[:, 1]
+        else:
+            raise ValueError(
+                "target_class parametr should ve one of {0, 1, None}"
+            )
--- credit_scoring/infer.py
+++ credit_scoring/infer.py
@@ -1,11 +1,6 @@
-import logging
-
-import numpy as np
-import pandas as pd
 from lightgbm import LGBMClassifier
 
 from credit_scoring.base_model import LGBM
-from credit_scoring.preprocessing import GetX
 from credit_scoring.utils import load_model
 
 
@@ -14,25 +9,6 @@ class LGBMInfer(LGBM):
         super().__init__(lgbt_params, cols)
         self.path_model_to_use = path_model_to_use
         self.model = self.load_model()
-
-    def predict_proba(
-        self, X_test: pd.DataFrame, target_class=None
-    ) -> np.ndarray:
-        X_test = GetX(self.tech_cols).fit_transform(X_test)
-        propability = self.model.predict_proba(X_test)
-
-        logging.info(f"Len of data is {len(propability)}")
-
-        if target_class is None:
-            return propability
-        elif target_class == 0:
-            return propability[:, 0]
-        elif target_class == 1:
-            return propability[:, 1]
-        else:
-            raise ValueError(
-                "target_class parametr should ve one of {0, 1, None}"
-            )
 
     def load_model(self) -> LGBMClassifier:
         lgb_classifier = load_model(self.path_model_to_use)

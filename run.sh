0a89502c434cab450ad3014427962b5d4ab4b165
gdemin <goshasty@yandex.ru>
2023-12-04 17:46:25 +0300

    add hypeopt to check, mlflow lock

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@ source ./.venv/bin/activate
 /home/gdemin/.local/bin/poetry install
 'pre-commit' install
 'pre-commit' run -a
+python3 commands.py hyperopt
+echo 'Hyperopt done'
 python3 commands.py train
+echo 'Train done'
 python3 commands.py infer
+echo 'Infer done'
 rm -r .venv/
 echo Done
